d6d907348e268bc1fbeef12b17eb17ff823759a1
Ashwin Bhargava <kunwarbhargava2000@gmail.com>
2022-04-02 16:27:05 +0530

    setup.sh added for automatic project setup

---
+++ setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+function clientSetup(){
+    cd client 
+    echo "$(tput setaf 2)Installing Client Dependencies"
+    npm install &> /dev/null
+    echo "$(tput setaf 2)Building Client"
+    npm run build &> /dev/null
+    cd ..
+}
+
+function serverSetup(){
+    cd server
+    echo "$(tput setaf 2)Installing Server Dependencies"
+    npm install &> /dev/null
+    cd ..
+}
+
+function startServer(){
+    echo "$(tput setaf 7) $(tput setab 0)"
+    cd server
+    node server
+}
+
+function main(){
+NODE=`type -P node`
+NPM=`type -P npm`
+
+if test -z "$NODE"
+then
+    echo "$(tput setaf 1) $(tput setab 7)Please install node to proceed further."
+    return 0;
+fi
+
+if test -z "$NPM" 
+then
+    echo "$(tput setaf 1) $(tput setab 7)Please install npm to proceed further."
+    return 0;
+fi
+
+clientSetup
+serverSetup
+
+echo "Start Server ? y/n"
+read $CHOICE
+
+if [[ $CHOICE -eq "y" || $CHOICE -eq "Y" ]]
+then
+echo "$(tput setaf 1) $(tput setab 7)THIS REPOSITORY REQUIRES NODE V14+.MAKE SURE ITS INSTALLED."
+startServer
+fi
+
+}
+
+main
+
